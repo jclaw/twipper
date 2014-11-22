9cf3677d9a68a25b9a16d8f376b963b3cff15289
jclaw <jclaw3456@gmail.com>
2014-11-22 16:58:44 -0500

    tweets and users associated

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,31 @@
 class TweetsController < ApplicationController
+	
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
-		@tweets = Tweet.all
+		@tweets = current_user.tweets
 	end
 
 	def create
 
 		@tweet = Tweet.new(tweet_params)
 		@tweet.user = current_user
-		@tweet.save
-		#@tweet
-		@tweet = Tweet.create(tweet_params)
-		@tweets = Tweet.all
-		flash[:success] = "You have created a tweet"
+		if @tweet.save
+			flash[:success] = "You have created a tweet"
+		else
+			flash[:danger] = "Please fix the problems below"
+
+		end
+		#@tweet = Tweet.create(tweet_params)
+		@tweets = current_user.tweets
+		
+			
 		render 'new'
+	end
+
+	def index
+		@tweets = Tweet.all.reject {|tweet| tweet.user == current_user}
 	end
 
 	private
